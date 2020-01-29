43d51cc9b8020bb986114436b6fbe4ad0e7ee827
“Chinedu <chinedu@zebranow.com>
2020-01-29 14:42:22 +0100

    Add product UI

---
+++ coder-swag/View/ProductCell.swift
@@ -0,0 +1,15 @@
+//
+//  ProductCell.swift
+//  coder-swag
+//
+//  Created by Ofor Chinedu on 29/01/2020.
+//  Copyright © 2020 Symple Innovations Company Limited. All rights reserved.
+//
+
+import UIKit
+
+class ProductCell: UICollectionViewCell {
+    @IBOutlet weak var productImage: UIImageView!
+    @IBOutlet weak var productTitle: UILabel!
+    @IBOutlet weak var productPrice: UILabel!
+}
