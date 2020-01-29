3f5bd26338de66a7a12e0464b852b05fbdddf0f8
“Chinedu <chinedu@zebranow.com>
2020-01-29 12:47:10 +0100

    add tables prototype

---
+++ coder-swag/Controller/CategoriesVC.swift
@@ -0,0 +1,22 @@
+//
+//  CategoriesVC.swift
+//  coder-swag
+//
+//  Created by Ofor Chinedu on 29/01/2020.
+//  Copyright © 2020 Symple Innovations Company Limited. All rights reserved.
+//
+
+import UIKit
+
+class CategoriesVC: UIViewController {
+    
+    @IBOutlet weak var categoryTable: UITableView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
